9371a25920a3e8536e894cd956db400bad82df32
Ivan Sergeev <82928893+Wailingray@users.noreply.github.com>
2021-11-09 18:12:14 +0300

    initial layout of header

--- src/components/appHeader/appHeader.tsx
+++ src/components/appHeader/appHeader.tsx
@@ -1,22 +1,35 @@
-import { Logo } from '@ya.praktikum/react-developer-burger-ui-components'
-import { BurgerIcon } from '@ya.praktikum/react-developer-burger-ui-components'
-import { ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components'
-import { ListIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+import { Logo } from '@ya.praktikum/react-developer-burger-ui-components';
+import { BurgerIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import { ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import { ListIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import styles from './appHeader.module.css';
 
 const AppHeader = () => {
   return (
-    <header className="header">
-      <div className="logo">
-        <Logo />
-        <h1 className="title">stellar burgers</h1>
-      </div>
-      <nav className="navigation">
-        <a href="" className="constructor-link">Конструктор</a>
-        <a href="" className="orders-link">Лента заказов</a>
-        <a href="" className="account">Личный кабинет</a>
+    <header className={styles.header}>
+      <div className={styles.logo}><Logo/></div>
+      <nav>
+        <ul className={styles.navigation}>
+          <li className={styles.item}>
+            <a href="" className={styles.link}>
+              <BurgerIcon type="primary"/>
+              <p className={styles.linkTitle}>Конструктор</p>
+            </a>
+          </li>
+          <li className={styles.item}>
+            <a href="" className={styles.link}>
+              <ListIcon type="primary"/>
+              <p className={styles.linkTitle}>Лента заказов</p>
+            </a>
+          </li>
+          <li className={styles.item}>
+            <a href="" className={styles.link}>
+              <ProfileIcon type="primary"/>
+              <p className={styles.linkTitle}>Личный кабинет</p>
+            </a>
+          </li>
+        </ul>
       </nav>
-
-
     </header>
   )
 }
