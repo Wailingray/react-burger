a980fc57ace9fd6429655e51832e24c83b0ebf11
Ivan Sergeev <82928893+Wailingray@users.noreply.github.com>
2021-11-13 12:39:42 +0300

    layout is complete

--- src/components/burgerConstructor/burgerConstructor.jsx
+++ src/components/burgerConstructor/burgerConstructor.jsx
@@ -1,63 +1,85 @@
 import React from "react";
 import styles from "./burgerConstructor.module.css";
-import { ConstructorElement } from '@ya.praktikum/react-developer-burger-ui-components'
+import { DragIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import { ConstructorElement } from "@ya.praktikum/react-developer-burger-ui-components";
+import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import { Button } from "@ya.praktikum/react-developer-burger-ui-components";
 
 const BurgerConstructor = (props) => {
-
   const makeAnArray = () => {
     const array = [];
     const objectArray = Object.entries(props);
     objectArray.forEach(([key, value]) => {
-    array.push(value);
+      array.push(value);
     });
     return array;
-  }
+  };
 
   const array = makeAnArray();
 
-  const renderProduct = (({name, image, price, _id},index,array) =>
-    {if (index===0) {
-      return <li key={_id} className={styles.ingridient}>
-        <ConstructorElement
-        type="top"
-        isLocked={true}
-        text={`${name} (верх)` }
-        price={price}
-        thumbnail={image}
-        />
-      </li>
-      }
-    else if (index===array.length-1) {
-      return <li key={_id} className={styles.ingridient}>
-        <ConstructorElement
-        type="bottom"
-        isLocked={true}
-        text={`${name} (низ)` }
-        price={price}
-        thumbnail={image}
-        />
-      </li>
+  const renderFirstProduct = ({ name, image, price, _id }, index, array) => {
+    if (index === 0) {
+      return (
+        <li key={index} className={`${styles.ingridient} pr-2`}>
+          <ConstructorElement
+            type="top"
+            isLocked={true}
+            text={`${name} (верх)`}
+            price={price}
+            thumbnail={image}
+          />
+        </li>
+      );
     }
-    else {
-      return <li key={_id} className={styles.ingridient}>
-        <ConstructorElement
-        isLocked={false}
-        text={name}
-        price={price}
-        thumbnail={image}
-        />
-      </li>
+  };
+
+  const renderLastProduct = ({ name, image, price, _id }, index, array) => {
+    if (index === array.length - 1) {
+      return (
+        <li key={index} className={`${styles.ingridient} pr-2`}>
+          <ConstructorElement
+            type="bottom"
+            isLocked={true}
+            text={`${name} (низ)`}
+            price={price}
+            thumbnail={image}
+          />
+        </li>
+      );
     }
+  };
+
+  const renderMidProducts = ({ name, image, price, _id }, index, array) => {
+    if (index !== 0 && index !== array.length - 1) {
+      return (
+        <li key={index} className={styles.ingridient}>
+          <DragIcon type="primary" />
+          <ConstructorElement
+            isLocked={false}
+            text={name}
+            price={price}
+            thumbnail={image}
+          />
+        </li>
+      );
     }
-  )
+  };
 
   return (
-    <section className={styles.section}>
-      <ul className={`${styles.ingridientList} mt-25`}>
-        {array.map(renderProduct)}
+    <section className={`${styles.section} pl-4 pr-2 pb-15`}>
+      <ul className={`${styles.ingridientList} mt-25 mb-10`}>
+        {array.map(renderFirstProduct)}
+        <ul className={styles.innerList}>{array.map(renderMidProducts)}</ul>
+        {array.map(renderLastProduct)}
       </ul>
+      <div className={`${styles.confirmationZone} mt-10`}>
+      <p className="text text_type_digits-medium">127890 <CurrencyIcon type="primary"/></p>
+      <Button type="primary" size="large">
+        Оформить заказ
+      </Button>
+      </div>
     </section>
   );
-}
+};
 
 export default BurgerConstructor;
--- src/components/burgerIngridients/burgerIngridients.jsx
+++ src/components/burgerIngridients/burgerIngridients.jsx
@@ -1,76 +1,91 @@
-import React from 'react';
-import styles from './burgerIngridients.module.css';
-import BurgerTabs from '../burgerTabs/burgerTabs';
-import Ingridient from '../ingridient/ingridient';
+import React from "react";
+import styles from "./burgerIngridients.module.css";
+import BurgerTabs from "../burgerTabs/burgerTabs";
+import Ingridient from "../ingridient/ingridient";
 
 const BurgerIngridients = (props) => {
   return (
     <section className={styles.section}>
-      <h1 className={`text text_type_main-large ${styles.title} pt-10 pb-5`}>Соберите бургер</h1>
+      <h1 className={`text text_type_main-large ${styles.title} pt-10 pb-5`}>
+        Соберите бургер
+      </h1>
       <BurgerTabs />
-      <div className={styles.layer}>
-        <h2 className={`text text_type_main-medium ${styles.component} pt-10 pb-6`}>Булки</h2>
-        <ul className={`${styles.list} pl-4 pr-2`}>
-          <li className="item">
-            <Ingridient count='1' {...props[0]}></Ingridient>
-          </li>
-          <li className="item">
-            <Ingridient {...props[14]}></Ingridient>
-          </li>
-        </ul>
+      <div className={styles.options}>
+        <div className={styles.layer}>
+          <h2
+            className={`text text_type_main-medium ${styles.component} pt-10 pb-6`}
+          >
+            Булки
+          </h2>
+          <ul className={`${styles.list} pl-4 pr-2`}>
+            <li className="item">
+              <Ingridient count="1" {...props[0]}></Ingridient>
+            </li>
+            <li className="item">
+              <Ingridient {...props[14]}></Ingridient>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.layer}>
+          <h2
+            className={`text text_type_main-medium ${styles.component} pt-10 pb-6`}
+          >
+            Соусы
+          </h2>
+          <ul className={`${styles.list} pl-4 pr-2`}>
+            <li className="item">
+              <Ingridient {...props[3]}></Ingridient>
+            </li>
+            <li className="item">
+              <Ingridient {...props[6]}></Ingridient>
+            </li>
+            <li className="item">
+              <Ingridient count="1" {...props[5]}></Ingridient>
+            </li>
+            <li className="item">
+              <Ingridient {...props[9]}></Ingridient>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.layer}>
+          <h2
+            className={`text text_type_main-medium ${styles.component} pt-10 pb-6`}
+          >
+            Начинки
+          </h2>
+          <ul className={`${styles.list} pl-4 pr-2`}>
+            <li className="item">
+              <Ingridient {...props[1]}></Ingridient>
+            </li>
+            <li className="item">
+              <Ingridient {...props[2]}></Ingridient>
+            </li>
+            <li className="item">
+              <Ingridient {...props[4]}></Ingridient>
+            </li>
+            <li className="item">
+              <Ingridient {...props[12]}></Ingridient>
+            </li>
+            <li className="item">
+              <Ingridient {...props[8]}></Ingridient>
+            </li>
+            <li className="item">
+              <Ingridient {...props[10]}></Ingridient>
+            </li>
+            <li className="item">
+              <Ingridient {...props[11]}></Ingridient>
+            </li>
+            <li className="item">
+              <Ingridient {...props[7]}></Ingridient>
+            </li>
+            <li className="item">
+              <Ingridient {...props[13]}></Ingridient>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className={styles.layer}>
-        <h2 className={`text text_type_main-medium ${styles.component} pt-10 pb-6`}>Соусы</h2>
-        <ul className={`${styles.list} pl-4 pr-2`}>
-          <li className="item">
-            <Ingridient {...props[3]}></Ingridient>
-          </li>
-          <li className="item">
-            <Ingridient {...props[6]}></Ingridient>
-          </li>
-          <li className="item">
-            <Ingridient count='1' {...props[5]}></Ingridient>
-          </li>
-          <li className="item">
-            <Ingridient {...props[9]}></Ingridient>
-          </li>
-        </ul>
-      </div>
-      <div className={styles.layer}>
-      <h2 className={`text text_type_main-medium ${styles.component} pt-10 pb-6`}>Начинки</h2>
-      <ul className= {`${styles.list} pl-4 pr-2`}>
-        <li className="item">
-          <Ingridient {...props[1]}></Ingridient>
-        </li>
-        <li className="item">
-          <Ingridient {...props[2]}></Ingridient>
-        </li>
-        <li className="item">
-          <Ingridient {...props[4]}></Ingridient>
-        </li>
-        <li className="item">
-          <Ingridient {...props[12]}></Ingridient>
-        </li>
-        <li className="item">
-          <Ingridient {...props[8]}></Ingridient>
-        </li>
-        <li className="item">
-          <Ingridient {...props[10]}></Ingridient>
-        </li>
-        <li className="item">
-          <Ingridient {...props[11]}></Ingridient>
-        </li>
-        <li className="item">
-          <Ingridient {...props[7]}></Ingridient>
-        </li>
-        <li className="item">
-          <Ingridient {...props[13]}></Ingridient>
-        </li>
-      </ul>
-    </div>
     </section>
-
-  )
-}
+  );
+};
 
 export default BurgerIngridients;
