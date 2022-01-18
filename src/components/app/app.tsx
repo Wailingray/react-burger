2ec1f72c98e57390c6ae9c32d510db3fc4d39df7
Ivan Sergeev <82928893+Wailingray@users.noreply.github.com>
2022-01-18 12:26:51 +0300

    first routing set-up

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -5,18 +5,26 @@ import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <Router>
       <AppHeader />
-      <DndProvider backend={HTML5Backend}>
-        <main className={styles.main}>
-          <BurgerIngredients />
-          <BurgerConstructor />
-        </main>
-      </DndProvider>
-    </>
+      <Routes>
+        <Route path="/">
+          <DndProvider backend={HTML5Backend}>
+            <main className={styles.main}>
+              <BurgerIngredients />
+              <BurgerConstructor />
+            </main>
+          </DndProvider>
+        </Route>
+        <Route path="/login">
+          <p>123123123</p>
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
