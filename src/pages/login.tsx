53593c6eeb18d3fdbf0f0106db8787c0041d86ce
Ivan Sergeev <82928893+Wailingray@users.noreply.github.com>
2022-01-19 11:32:03 +0300

    finished creating login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -9,36 +9,47 @@ export const LoginPage: React.FC = () => {
     <div className={`${styles.formContainer}`}>
       <form className={styles.form} action="">
         <p className="text text_type_main-medium mb-6">Вход</p>
-        <div className={`${styles.inputContainer} mb-6`}>
+        <form className={`${styles.inputContainer} mb-6`}>
           <Input
             value={""}
             name={"email"}
             size="default"
             placeholder="E-mail"
-            onChange={e => (e.target.value)}
+            onChange={(e) => e.target.value}
           />
-        </div>
-        <div className={`${styles.inputContainer} mb-6`}>
-          <Input
+        </form>
+        <form className={`${styles.inputContainer} mb-6`}>
+          <PasswordInput
             value={""}
             name={"password"}
             size="default"
-            placeholder="Пароль"
-            onChange={e => (e.target.value)}
+            onChange={(e) => e.target.value}
           />
-        </div>
-        <Button type="primary" size="large">Войти</Button>
-        <div className={`${styles.link} mt-20 mb-4`}>
+        </form>
+        <Button type="primary" size="large">
+          Войти
+        </Button>
+        <div className={`${styles.linkContainer} mt-20 mb-4`}>
           <p className="text text_type_main-default text_color_inactive">
-            The quick brown fox .
+            Вы — новый пользователь?
           </p>
-          <a href="">jumps over the lazy dog</a>
+          <a
+            href=""
+            className={`${styles.link} text text_type_main-default text_color_inactive`}
+          >
+            Зарегистрироваться
+          </a>
         </div>
-        <div className={`${styles.link} mb-4`}>
+        <div className={`${styles.linkContainer} mb-4`}>
           <p className="text text_type_main-default text_color_inactive">
-            The quick brown fox .
+            Забыли пароль?
           </p>
-          <a href="">jumps over the lazy dog</a>
+          <a
+            href=""
+            className={`${styles.link} text text_type_main-default text_color_inactive`}
+          >
+            Восстановить пароль
+          </a>
         </div>
       </form>
     </div>
