a36810ecdb6523eb782827ca5f7709e57e2107b5
Ivan Sergeev <82928893+Wailingray@users.noreply.github.com>
2022-02-13 19:09:51 +0300

    Fixed background issue

--- src/components/HOC/protected-route.tsx
+++ src/components/HOC/protected-route.tsx
@@ -1,18 +1,24 @@
-import React from 'react';
-import { Redirect, Route, useLocation } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import { useAppDispatch, useAppSelector } from '../../services/hooks/hooks';
-import { dispatchGetUser } from '../../services/actions/user';
-import { ProtectedRouteProps, TLocationState } from '../../services/utils/interfaces';
-import { wsConnectionClosed, wsConnectionStart } from '../../services/actions/wsActions';
-import { getItems } from '../../services/actions/ingredients';
+import React from "react";
+import { Redirect, Route, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "../../services/hooks/hooks";
+import { dispatchGetUser } from "../../services/actions/user";
+import {
+  ProtectedRouteProps,
+  TLocationState,
+} from "../../services/utils/interfaces";
+import {
+  wsConnectionClosed,
+  wsConnectionStart,
+} from "../../services/actions/wsActions";
+import { getItems } from "../../services/actions/ingredients";
 
-export const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children, path, exact}) => {
-
-  const dispatch = useAppDispatch()
-  const { user } = useAppSelector(
-    (state) => state.user
-  );
+export const ProtectedRoute: React.FC<ProtectedRouteProps> = ({
+  children,
+  ...rest
+}) => {
+  const dispatch = useAppDispatch();
+  const { user } = useAppSelector((state) => state.user);
   const [isUserLoaded, setUserLoaded] = useState(false);
 
   const init = async () => {
@@ -24,26 +30,21 @@ export const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children, path, 
     init();
   }, []);
 
-  if (!isUserLoaded) {
-    return null;
-  }
-
-  return (
-    <Route
-      path={path}
-      exact={exact}
-      render={({ location }) =>
-        user ? (
-          children
-        ) : (
+  if (!user) {
+    return (
+      <Route
+        {...rest}
+        render={({ location }) => (
           <Redirect
             to={{
-              pathname: '/login',
-              state: { from: location }
+              pathname: "/login",
+              state: { from: location },
             }}
           />
-        )
-      }
-    />
-  );
-}
+        )}
+      />
+    );
+  }
+
+  return <Route {...rest} render={() => children} />;
+};
